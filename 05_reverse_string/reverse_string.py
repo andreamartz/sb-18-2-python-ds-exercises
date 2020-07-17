d6e765af69b0267ce836d1f1dcbb163d3a3a632f
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-07-17 15:39:04 -0500

    Complete 05_reverse_string exercise

--- 05_reverse_string/reverse_string.py
+++ 05_reverse_string/reverse_string.py
@@ -7,3 +7,28 @@ def reverse_string(phrase):
         >>> reverse_string('sauce')
         'ecuas'
     """
+
+    return phrase[::-1]
+
+    #========================================
+    # THE LONG WAY!
+    #========================================
+
+    #****************************************
+    # split the string into characters (returns a list?)
+    #****************************************
+    # phrase_list = list(phrase)
+
+    #****************************************
+    # iterate over the list in reverse order and create a new list
+    #****************************************
+    # index = len(phrase_list) - 1
+    # phrase_list_reverse = []
+    # while index >= 0:
+    #     phrase_list_reverse.append(phrase_list[index])
+    #     index -= 1
+    #****************************************
+    # join the characters back together & return
+    #****************************************
+    # return "".join(phrase_list_reverse)
+

06e3d02abf68639cbca1c3a9d3f088d785480b7a
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-07-18 11:50:25 -0500

    Complete flip_case exercise

--- 11_flip_case/flip_case.py
+++ 11_flip_case/flip_case.py
@@ -11,3 +11,28 @@ def flip_case(phrase, to_swap):
         'AaaaHHH'
 
     """
+    # iterate over the phrase
+    # for every char that matches to_swap,
+    # change the case of that char using str.swapcase()
+
+    idx = 0
+    phrase_list = list(phrase)
+    while idx < len(phrase_list):
+        if phrase_list[idx] == to_swap.lower() or phrase_list[idx] == to_swap.upper():
+            phrase_list[idx] = phrase_list[idx].swapcase()
+        idx += 1
+    phrase = "".join(phrase_list)
+    return phrase
+
+    # ****************************************
+    # SB's SOLUTION
+    # ****************************************
+    # to_swap = to_swap.lower()
+    # out = ""
+
+    # for ltr in phrase:
+    #     if ltr.lower() == to_swap:
+    #         ltr = ltr.swapcase()
+    #     out += ltr
+
+    # return out

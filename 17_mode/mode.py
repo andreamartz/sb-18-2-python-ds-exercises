3b920502016a3bb6bc83e69523a44bba3d511c44
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-07-19 10:27:25 -0500

    Complete 17-mode exercise

--- 17_mode/mode.py
+++ 17_mode/mode.py
@@ -11,3 +11,31 @@ def mode(nums):
         >>> mode([2, 2, 3, 3, 2])
         2
     """
+    # create a dictionary with entries of the form num: count
+    counts = {num: nums.count(num) for num in nums}
+    # find the max frequency
+    max_value = max(counts.values())
+    # find the key in the original dictionary that has that count
+    mode_list = [num for num, freq in counts.items() if freq == max_value]
+
+    return mode_list[0]
+
+    # ************************************
+    # SB's SOLUTION
+    # ************************************
+
+    # Make frequency counter of num:freq
+    # counts = {}
+
+    # for num in nums:
+    # counts[num] = counts.get(num, 0) + 1
+
+    # find the highest value (the most frequent number)
+
+    # max_value = max(counts.values())
+
+    # now we need to see at which index the highest value is at
+
+    # for (num, freq) in counts.items():
+    #     if freq == max_value:
+    #         return num

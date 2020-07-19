4e69f1f12f8fd692eeae18e7fc56d5e058c6efd4
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-07-19 15:05:54 -0500

    Complete 27-titleize exercise

--- 27_titleize/titleize.py
+++ 27_titleize/titleize.py
@@ -7,3 +7,16 @@ def titleize(phrase):
         >>> titleize('oNLy cAPITALIZe fIRSt')
         'Only Capitalize First'
     """
+    phrase = phrase.split(" ")
+    phrase_list = [f"{word.capitalize()}" for word in phrase]
+    return " ".join(phrase_list)
+
+    # *********************
+    # SB's SOLUTION
+    # *********************
+    # there's a built-in method for this!
+
+    # return phrase.title()
+
+    # or, if you didn't know that, could capitalize each word by hand
+    # return ' '.join([s.capitalize() for s in phrase.split(' ')])
